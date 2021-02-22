fee7369eb08fc237f9265c046cf50a916c6f8059
Stéphane Galland <galland@arakhne.org>
2021-02-22 11:04:57 +0100

    Minor changes.

---
+++ src-tests/fr/utbm/gl52/stack/tests/ArrayStackTest.java
@@ -0,0 +1,19 @@
+package fr.utbm.gl52.stack.tests;
+
+/** Test of {@code ArrayStack}.
+ *
+ * @author sgalland
+ */
+@SuppressWarnings("javadoc")
+public class ArrayStackTest {
+
+	public void push() throws Exception {
+	}
+
+	public void pop() throws Exception {
+	}
+
+	public void size() throws Exception {
+	}
+
+}
--- src/module-info.java
+++ src/module-info.java
@@ -1,7 +1,10 @@
-/** Stack implementation.
+/** GL52 implementation.
  *
  * @author sgalland
  */
-module fr.utbm.gl52.stack {
+module fr.utbm.gl52.tps {
 	exports fr.utbm.gl52.stack;
+	requires org.mockito;
+	requires org.junit.jupiter.api;
+	requires org.junit.platform.commons;
 }
